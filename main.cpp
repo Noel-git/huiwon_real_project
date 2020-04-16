545f146a8e1b97cce8cf724239502cb6c1733f0c
Noel-git <21300717@handong.edu>
2020-04-16 02:33:50 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include "sorting.h"
+using namespace std;
 
+int sort[10] = {};
 int sorted[10];
+int t = 0;
+
 
 int main(){
+    for(int i = 0; i < 10; i++){
+        cin >> t;
+        sort[i] = int(t);
+    }
+    merge_sort(sort, 0, 9);
 
-    return 0;
+    for(int i = 0; i < 10; i++){
+        cout << sorted[i] << ' ';
+    }
+    cout << '\n';
+    return 0; 
 }
